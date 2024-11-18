fa4331cea26adf77ebb65f398140e0b7e4db1d9b
Romy Gersht <romygersht@Romys-MacBook-Air.local>
2024-11-18 23:51:44 +0200

    hw2

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,39 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        
+        String str1 = "" +(args[0]); //the string input
+        int num1 = Integer.parseInt(args[1]); //numer input
+        String str2 = "AEFHILMNORSX";
+        String capital = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";//creating a string including all the letters from the ABC
+        String notCapital = "abcdefghijklmnopqrstuvwsyz";
+
+
+           for (int l = 0; l<str1.length(); l++) {
+                char ch = str1.charAt(l); //creating a char for the letter
+                int i = 0; 
+                i = notCapital.indexOf(ch);
+                ch = capital.charAt(i);
+                    
+
+                if (str2.indexOf(ch)== -1) // if the letters in the str2 does not appear
+        {      
+                System.out.println("Give me a: "+ch+"!");
+                System.out.println("what does that spell?");
+                
         }
-}
+                else // if they do apear
+        {
+                System.out.println("Give me an: "+ch+"!"); 
+                System.out.println("what does that spell?");
+        }
+                ch = str1.charAt(l); //+1 at the string
+           }
+           while (num1 >0) { //printing the word as many times as the num
+                System.out.println(str1+ "!!!");
+                num1 --;
+                }
+        }
+        
+  }
+
