5bea385c1e4ce739cbc6f42e7371eaf8e5f2edbf
Romy Gersht <romygersht@Romys-MacBook-Air.local>
2024-11-19 00:08:17 +0200

    hw2

--- Cheers.java
+++ Cheers.java
@@ -7,14 +7,14 @@ public class Cheers {
         String str2 = "AEFHILMNORSX";
         String capital = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";//creating a string including all the letters from the ABC
         String notCapital = "abcdefghijklmnopqrstuvwsyz";
-
+        String str3 = " ";
 
            for (int l = 0; l<str1.length(); l++) {
                 char ch = str1.charAt(l); //creating a char for the letter
                 int i = 0; 
                 i = notCapital.indexOf(ch);
                 ch = capital.charAt(i);
-                    
+                str3 = str3 +ch;
 
                 if (str2.indexOf(ch)== -1) // if the letters in the str2 does not appear
         {      
@@ -29,8 +29,8 @@ public class Cheers {
         }
                 ch = str1.charAt(l); //+1 at the string
            }
-           while (num1 >0) { //printing the word as many times as the num
-                System.out.println(str1+ "!!!");
+           while (num1 >0) { //printing the word as many times as the num // להפוך לאותיות גדולות
+                System.out.println(str3+ "!!!");
                 num1 --;
                 }
         }
