8b4f7f27aab57a31b66f85f26730a31a25f94a0e
Romy Gersht <romygersht@Romys-MacBook-Air.local>
2024-11-19 20:29:28 +0200

    hw2

--- CalcPi.java
+++ CalcPi.java
@@ -5,16 +5,15 @@ public class CalcPi {
 	   
     int num = Integer.parseInt(args[0]); //numer input
 	double sum = 0; //the sum of the elements
-	int one = 1;
+	double one = 1;
 
-	for (int i=0; i <= num; i++) { // a loop that sums the elements as many times as the number in the input
+	for (int i=0; i < num; i++) { // a loop that sums the elements as many times as the number in the input
 	    double element = one / ((2*i) + 1); //calculate the specipic element 
 		sum += element;
 		one *= -1;
-
 	}
 
-	System.out.println("pi according to java:  " + sum * 4); //the answer*4 because it returns pai/4
-	System.out.println("pi, approximated:      " + Math.PI);
+	System.out.println("pi according to java:  " + Math.PI); //the answer*4 because it returns pai/4
+	System.out.println("pi, approximated:      " + sum*4);
 	}
 }
