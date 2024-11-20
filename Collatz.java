0a367cd96df815be90cd1962f1392d332eb5ec4c
Romy Gersht <romygersht@Romys-MacBook-Air.local>
2024-11-20 08:22:23 +0200

    hw2

--- Collatz.java
+++ Collatz.java
@@ -6,31 +6,39 @@ public class Collatz {
 		String mode = ""+(args[1]);
 		//int time = seed;
 		int num = seed;
-		int cNumber = 0;
+		//int cNumber = 2;
 
 		String v = "v";
 
 		if (mode.equals(v)) { 
+			//int counter = 0;
 
-			if (j == 1){
-				System.out.print("1 4 2 1 (4)");
-			}
+			//if ( == 1){
+			System.out.println("1 4 2 1 (4)");
+			//counter ++;
+			//System.out.println("Every one of the first "+num+" hailstone sequences reach 1.");
+			//}
+			//else {
+			//System.out.print("1");
+			//counter ++;
+			//}
 
-           for (int j=1; j <=seed; j++) {
+           for (int j=2; j <=seed; j++) {
 			int counter = 0;
+			int cNumber = j;
+			System.out.print(+j+" ");
+			counter++;
 
-		     while (j!=1) {
-
+		     while (cNumber!=1) {
 		    if (cNumber%2 == 0){	
-			cNumber = j/2;
+			cNumber = cNumber/2;
 			System.out.print(+cNumber+" ");
-			counter ++;
 		    }
 		     else{
 			cNumber = (cNumber*3)+1;
 			System.out.print(+cNumber+" ");
-			counter ++;
-		    }  
+		    }
+			counter ++;  
 	       }
 		   System.out.println(" ("+counter+")"); 
 	   }
@@ -40,6 +48,9 @@ public class Collatz {
 		else {
 		System.out.println("Every one of the first "+num+" hailstone sequences reach 1.");
 		}
+
+
+
 	    }
 	  }
 
